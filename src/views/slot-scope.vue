e5f56163940522595093bf2b6d4317f701ba4d91
MarioGogogo <148373644@qq.com>
2021-05-27 15:56:30 +0800

    slot 插槽作用域

---
+++ src/views/slot-scope.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h5>slotscope</h5>
+    <ul>
+      <li v-for="book in books" :key="book.id">
+        <slot :book="book">
+          <!-- 默认内容 -->
+          {{ book.name }}
+        </slot>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "slotscope",
+  data() {
+    return {
+      books: [{id:"001",name:"一"},{id:"002",name:"二"}]
+    }
+  },
+}
+</script>
+
+<style lang="sass" scoped>
+</style>
